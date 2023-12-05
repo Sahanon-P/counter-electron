b60482df01754563f0c4393fbaa4ee10f8a6db75
Sahanon Phisetpakasit <pingotakulml@gmail.com>
2023-12-05 16:46:41 +0700

    optimize button and add endless mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import {
   Box,
   Button,
+  FormControlLabel,
   MenuItem,
   Stack,
+  Switch,
   TextField,
   Typography,
 } from "@mui/material";
@@ -13,7 +15,7 @@ import Message from "./Message";
 const time = [
   {
     label: "1 min",
-    value: "01"
+    value: "01",
   },
   {
     label: "45 min",
@@ -35,7 +37,7 @@ const maximumKill = [
     value: 50,
   },
   {
-    label: "30 kills",
+    label: "70 kills",
     value: 70,
   },
 ];
@@ -47,26 +49,29 @@ function App() {
   const [open, setOpen] = useState(false);
   const [message, setMessage] = useState("");
   const [complete, setComplete] = useState(true);
-  const [max, setMax] = useState(15);
+  const [max, setMax] = useState(50);
   const [toggle, setToggle] = useState(true);
+  const [endless, setEndless] = useState(false);
   function handleStart() {
     setComplete(false);
   }
   function handleReset() {
-    window.location.reload()
+    window.location.reload();
   }
-  function resetScore(){
+  function resetScore() {
     setP1(0);
     setP2(0);
   }
+  const endlessMode = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEndless(event.target.checked);
+  };
   useEffect(() => {
     if (p1 === max || p2 === max) {
       if (p1 === max) {
-        setMessage(toggle ? 'blue win' : 'red win');
+        setMessage(toggle ? "blue win" : "red win");
         setOpen(true);
-      }
-      else if (p2 === max) {
-        setMessage(toggle ? 'red win' : 'blue win');
+      } else if (p2 === max) {
+        setMessage(toggle ? "red win" : "blue win");
         setOpen(true);
       }
       setSecond("00");
@@ -76,139 +81,184 @@ function App() {
     }
   }, [p1, p2]);
   useEffect(() => {
-    if (!complete) {
-      const interval = setInterval(() => {
-        if (parseInt(second) > 0) {
-          if (parseInt(second) < 11) {
-            setSecond(`0${parseInt(second) - 1}`);
-          } else {
-            setSecond(`${parseInt(second) - 1}`);
-          }
-        }
-        if (parseInt(second) === 0) {
-          if (parseInt(minute) === 0) {
-            setComplete(true);
-            if (p1 > p2){
-              setMessage(toggle ? 'blue win' : 'red win');
-            }
-            else if (p2 > p1) {
-              setMessage(toggle ? 'red win' : 'blue win');
-            }
-            resetScore();
-            setOpen(true);
-            setMinute("45");
-            clearInterval(interval);
-
-          } else {
-            if (parseInt(minute) < 10) {
-              setMinute(`0${parseInt(minute) - 1}`);
+    if (!endless) {
+      if (!complete) {
+        const interval = setInterval(() => {
+          if (parseInt(second) > 0) {
+            if (parseInt(second) < 11) {
+              setSecond(`0${parseInt(second) - 1}`);
             } else {
-              setMinute(`${parseInt(minute) - 1}`);
+              setSecond(`${parseInt(second) - 1}`);
             }
-            setSecond("59");
           }
-        }
-      }, 1000);
-      return () => {
-        clearInterval(interval);
-      };
+          if (parseInt(second) === 0) {
+            if (parseInt(minute) === 0) {
+              setComplete(true);
+              if (p1 > p2) {
+                setMessage(toggle ? "blue win" : "red win");
+              } else if (p2 > p1) {
+                setMessage(toggle ? "red win" : "blue win");
+              }
+              resetScore();
+              setOpen(true);
+              setMinute("45");
+              clearInterval(interval);
+            } else {
+              if (parseInt(minute) < 10) {
+                setMinute(`0${parseInt(minute) - 1}`);
+              } else {
+                setMinute(`${parseInt(minute) - 1}`);
+              }
+              setSecond("59");
+            }
+          }
+        }, 1000);
+        return () => {
+          clearInterval(interval);
+        };
+      }
     }
   });
   return (
     <Stack>
       <Stack alignItems={"center"} spacing={3}>
         <img src={Logo} height={200} width={400} />
-        <Typography variant="h4" sx={{ fontWeight: "bold", color: "white" }}>
-          00:{minute}:{second}
+        {!endless ? (
+          <Typography variant="h4" sx={{ fontWeight: "bold", color: "white" }}>
+            00:{minute}:{second}
+          </Typography>
+        ) : (
+          <Typography variant="h4" sx={{ fontWeight: "bold", color: "white" }}>
+          Endless Mode
         </Typography>
-        <Stack direction={'row'} spacing={10}>
+        )}
+        <Stack direction={"row"} spacing={10}>
           <Box
             width={400}
             height={400}
-            bgcolor={!toggle ? 'error.main' : 'primary.main'}
+            bgcolor={!toggle ? "error.main" : "primary.main"}
             textAlign={"center"}
           >
             <Typography fontSize={200}>{p1}</Typography>
           </Box>
           {complete ? (
-            <Button variant="contained" color="success" onClick={() => { setToggle(!toggle) }}>
+            <Button
+              variant="contained"
+              color="success"
+              onClick={() => {
+                setToggle(!toggle);
+              }}
+            >
               Switch
             </Button>
           ) : (
-            <Typography color="white" variant="h1">{max}</Typography>
+            <Typography color="white" variant="h1">
+              {max}
+            </Typography>
           )}
           <Box
             width={400}
             height={400}
-            bgcolor={toggle ? 'error.main' : 'primary.main'}
+            bgcolor={toggle ? "error.main" : "primary.main"}
             textAlign={"center"}
           >
             <Typography fontSize={200}>{p2}</Typography>
           </Box>
         </Stack>
         {complete ? (
-          <Stack direction={"row"} spacing={2}>
-            <TextField
-              select
-              defaultValue={"45"}
-              sx={{ bgcolor: "white" }}
-              onChange={(event) => {
-                setMinute(event.target.value);
-              }}
-            >
-              {time.map((row, index) => (
-                <MenuItem key={index} value={row.value}>{row.label}</MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              select
-              defaultValue={50}
-              sx={{ bgcolor: "white" }}
-              onChange={(event) => {
-                setMax(parseInt(event.target.value));
-              }}
-            >
-              {maximumKill.map((row, index) => (
-                <MenuItem key={index} value={row.value}>{row.label}</MenuItem>
-              ))}
-            </TextField>
+          <Stack alignItems={"center"} spacing={2}>
+            <FormControlLabel
+              control={
+                <Switch checked={endless} size="medium" color="error" onChange={endlessMode} />
+              }
+              label={<Typography color={"white"}>Endless</Typography>}
+              labelPlacement="end"
+            />
+            <Stack direction={"row"} spacing={2}>
+              {!endless ? (
+                <TextField
+                  select
+                  defaultValue={"45"}
+                  onChange={(event) => {
+                    setMinute(event.target.value);
+                  }}
+                >
+                  {time.map((row, index) => (
+                    <MenuItem key={index} value={row.value}>
+                      {row.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              ) : (
+                <></>
+              )}
+              <TextField
+                select
+                defaultValue={50}
+                onChange={(event) => {
+                  setMax(parseInt(event.target.value));
+                }}
+              >
+                {maximumKill.map((row, index) => (
+                  <MenuItem key={index} value={row.value}>
+                    {row.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Stack>
           </Stack>
         ) : (
           <></>
         )}
         {complete ? (
-          <Stack direction={'row'} spacing={5}>
+          <Stack direction={"row"} spacing={5}>
             <Button variant="contained" color="primary" onClick={handleStart}>
               Start
             </Button>
-            <Button variant="contained" color="success" onClick={handleReset}>
+            <Button variant="contained" color="secondary" onClick={handleReset}>
               Reset
             </Button>
           </Stack>
         ) : (
-          <Stack direction={'row'} spacing={10}>
-            <Button variant="contained" color={!toggle ? 'error' : 'primary'} tabIndex={0} onKeyDown={(event) => {
-              if (event.key === "Enter") {
-                event.preventDefault();
-                setP1(p1 + 1);
-              }
-              else if (event.key === "Spacebar" || event.key === ' '){
-                event.preventDefault();
-                setP2(p2+1);
-              }
-            }}>
+          <Stack direction={"row"} spacing={10}>
+            <Button
+              variant="contained"
+              color={!toggle ? "error" : "primary"}
+              tabIndex={0}
+              onKeyUp={(event) => {
+                if (event.key === "Enter") {
+                  event.preventDefault();
+                  setP1(p1 + 1);
+                } else if (event.key === "Spacebar" || event.key === " ") {
+                  event.preventDefault();
+                  setP2(p2 + 1);
+                }
+              }}
+            >
               Score
             </Button>
-            <Button variant="contained" color="secondary" onClick={() => { setComplete(true) }}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                setComplete(true);
+              }}
+            >
               Stop
             </Button>
-            <Button variant="contained" color={toggle ? 'error' : 'primary'} >
+            <Button variant="contained" color={toggle ? "error" : "primary"}>
               Score
             </Button>
           </Stack>
         )}
       </Stack>
-      <Message open={open} handleClose={() => {setOpen(false)}} message={message}/>
+      <Message
+        open={open}
+        handleClose={() => {
+          setOpen(false);
+        }}
+        message={message}
+      />
     </Stack>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+export const themeOptions = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f59700',
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+     <ThemeProvider theme={themeOptions}>
+     <App />
+     </ThemeProvider>
   </React.StrictMode>,
 )
 
